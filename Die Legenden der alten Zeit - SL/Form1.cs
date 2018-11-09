0e153a0b9a71ea578934bd77f59a81be0ee14dcc
HawkeyeGreen <thymiana@web.de>
2018-11-09 18:57:36 +0100

    Projektdateien hinzufügen.

---
+++ Die Legenden der alten Zeit - SL/Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Die_Legenden_der_alten_Zeit___SL
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
