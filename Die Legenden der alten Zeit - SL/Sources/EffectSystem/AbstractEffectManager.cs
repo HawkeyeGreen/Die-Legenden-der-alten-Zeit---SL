802079914e770c1e7d8da6e92e83f4938dd84850
HawkeyeGreen <thymiana@web.de>
2018-11-23 18:22:31 +0100

    EffectSystem-Basis

---
+++ Die Legenden der alten Zeit - SL/Sources/EffectSystem/AbstractEffectManager.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Die_Legenden_der_alten_Zeit___SL.Sources.EffectSystem
+{
+    /**
+     * Klasse: AbstractEffectManager
+     * Der EffectManager wird für verschiedene Typen von effektbetreffbaren Dingen abgeleitet.
+     * Dabei ist der EffeCtManager zuständig für das Einstellen und Auslesen von Effekten. Er verwendet dafür das
+     * GlobalEffeCtManagement, entscheidet aber nicht, ob ein Effekt trifft oder nicht.
+     * 
+     * Diese Entscheidungen muss der Nutzer des EffectManager treffen. Er handelt mit seinen Zielen aus, ob sie - wovon immer - getroffen werden können.
+     * Dann nutzt der EffektWirker seinen EffectManager
+     */
+    class AbstractEffectManager
+    {
+
+    }
+}
---
+++ Die Legenden der alten Zeit - SL/Sources/EffectSystem/GlobalEffectSystem.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Die_Legenden_der_alten_Zeit___SL.Sources.EffectSystem
+{
+    class GlobalEffectSystem
+    {
+    }
+}
