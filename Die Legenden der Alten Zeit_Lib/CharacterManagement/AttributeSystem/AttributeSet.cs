db12fc2ce294ec938aa20b923970e51bbd9d9a2b
HawkeyeGreen <thymiana@web.de>
2018-12-02 14:19:42 +0100

    attributeset implementiert

---
+++ Die Legenden der Alten Zeit_Lib/CharacterManagement/AttributeSystem/AttributeSet.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Die_Legenden_der_Alten_Zeit_Lib.CharacterManagement.AttributeSystem
+{
+    /// <summary>
+    /// Diese Klasse enthält eine Menge von Attributen. Es können manuell Attribute registriert werden oder
+    /// via eines Standardattributs.
+    /// </summary>
+    public class AttributeSet
+    {
+        private Dictionary<string, int> attributes = new Dictionary<string, int>();
+        private Dictionary<string, string> attributeLinking = new Dictionary<string, string>();
+        private bool locked = false;
+
+        /// <summary>
+        /// Initialisiert das AttributSet mit Hilfe einer Liste von Standardattributen und Werten.
+        /// </summary>
+        /// <param name="standardAttributes">Eine Liste von Standardattribute-Attributwert-Paaren.</param> 
+        /// <param name="lockSet">Ermöglicht es das AttributeSet direkt zu schließen. Standard: offen.</param>
+        public AttributeSet(List<Tuple<StandardAttribute, int>> standardAttributes, bool lockSet = false)
+        {
+            foreach(Tuple<StandardAttribute, int> attribute in standardAttributes)
+            {
+                ImportStandardAttribute(attribute.Item1, attribute.Item2);
+            }
+            locked = lockSet;
+        }
+
+        /// <summary>
+        /// Initialisiert die Liste mit einer Liste von Attributnamen, Attributwerten und Attributlinks.
+        /// </summary>
+        /// <param name="attributes"></param>
+        /// <param name="lockSet"></param>
+        public AttributeSet(List<Tuple<string, int, List<string>>> attributes, bool lockSet = false)
+        {
+            foreach(Tuple<string, int, List<string>> _Attribute in attributes)
+            {
+                ImportStandardAttribute(new StandardAttribute(_Attribute.Item1, _Attribute.Item3), _Attribute.Item2);
+            }
+        }
+
+        /// <summary>
+        /// Importiert ein Standardattribut in dieses AttributeSet und intialisiert es mit dem gegebenen Wert.
+        /// Sollte das Attribut bereits im Set sein, dann wird sein aktueller Wert mit dem Initialwert überschrieben,
+        /// außer wenn das AttributeSet 'gelocked' ist, dann wird 'False' zurückgegeben.
+        /// </summary>
+        /// <param name="attribute">Das Standardattribute - es enthält sowohl die korrekte Definition des Attributs als auch alle Verlinkungen.</param>
+        /// <param name="value">Der Initialwert des Attributs.</param>
+        /// <returns></returns>
+        public void ImportStandardAttribute(StandardAttribute attribute, int value)
+        {
+            if(ContainsAttribute(attribute.Key))
+            {
+                // Offenbar ist das Attribut bereits vorhanden
+                // Setze es regulär auf den neuen Wert
+                SetAttributeValue(attribute.Key, value);
+            }
+            else
+            {
+                // Attribut existiert noch nicht -> lege es an
+                attributes.Add(attribute.Key, value);
+            }
+
+            // Fügt jeden referenzierten Schlüssel des Standardattributs in das Dictionary der attributeLinks ein.
+            // Wenn ein Schlüssel bereits verlinkt, dann wird der Link überschrieben.
+            attribute.ResetLookIndex();
+            Tuple<bool, string> refStringTuple = attribute.ReadReferencedKey();
+            while (refStringTuple.Item1)
+            {
+                if (!attributeLinking.ContainsKey(refStringTuple.Item2))
+                {
+                    attributeLinking.Add(refStringTuple.Item2, attribute.Key);
+                }
+                else
+                {
+                    attributeLinking[refStringTuple.Item2] = attribute.Key;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Gibt true zurück, sollte das AttributeSet das angegebene Attribut oder ein verlinktes Äquivalent enthalten.
+        /// </summary>
+        /// <param name="attributeKey">Der Key, nachdem gesucht werden soll.</param>
+        /// <returns></returns>
+        public bool ContainsAttribute(string attributeKey)
+        {
+            if(attributes.ContainsKey(attributeKey))
+            {
+                return true;
+            }
+            else if(attributeLinking.ContainsKey(attributeKey))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// Setzt das angebene Attribut auf den angegebenen Wert.
+        /// Diese Methode macht nichts, sollte das Set kein Attribut unter dem gegebenen Schlüssel enthalten.
+        /// </summary>
+        /// <param name="key"></param>
+        /// <param name="value"></param>
+        public void SetAttributeValue(string attributeKey, int value)
+        {
+            if (attributes.ContainsKey(attributeKey))
+            {
+                attributes[attributeKey] = value;
+            }
+            else if (attributeLinking.ContainsKey(attributeKey))
+            {
+                attributes[attributeLinking[attributeKey]] = value;
+            }
+        }
+    }
+}
---
+++ Die Legenden der Alten Zeit_Lib/CharacterManagement/AttributeSystem/StandardAttribute.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Zeus.Hermes;
+using Die_Legenden_der_Alten_Zeit_Lib.DB_Management;
+
+namespace Die_Legenden_der_Alten_Zeit_Lib.CharacterManagement.AttributeSystem
+{
+    /// <summary>
+    /// Diese Klasse verpackt ein Standardattributnamen und alle auf dieses Attribut verweisende Referenzen.
+    /// </summary>
+    public class StandardAttribute : HermesLoggable
+    {
+        private int _ID;
+        private string attributeKey;
+        private List<string> linkedKeys;
+        private int currentLookIndex = 0;
+
+        /// <summary>
+        /// Der Schlüssel des Attributs unter dem es primär angesprochen werden soll.
+        /// </summary>
+        public string Key
+        {
+            get => attributeKey;
+            set => attributeKey = value;
+        }
+
+        /// <summary>
+        /// Attributsnamen, die auf dieses Attribut verlinkt werden sollen.
+        /// </summary>
+        public List<string> LinkedKeys
+        {
+            get => linkedKeys;
+            set => linkedKeys = value;
+        }
+
+        public long ID => Convert.ToInt64(_ID);
+
+        public string Type => "StandardAttribute";
+
+        /// <summary>
+        /// Dieser Konstruktor ermöglicht es ausschließlich den Key festzulegen.
+        /// Alle Referenzen müssen nachträglich hinzugefügt werden.
+        /// </summary>
+        /// <param name="key">Der Attributsschlüssel. Beispiele: "ST" für Stärke; "GE" für Geschicklichkeit</param>
+        public StandardAttribute(string key)
+        {
+            attributeKey = key;
+            linkedKeys = new List<string>();
+        }
+
+        /// <summary>
+        /// In diesem Konstruktor wird die Liste der referenzierten Schlüsselnamen gemeinsam mit dem Attributsschlüssel übergeben.
+        /// </summary>
+        /// <param name="key">Der Attributsschlüssel. Beispiele: "ST" für Stärke; "GE" für Geschicklichkeit</param>
+        /// <param name="referenced">Die verwiesenen Schlüssel.</param>
+        public StandardAttribute(string key, List<string> referenced)
+        {
+            attributeKey = key;
+            linkedKeys = referenced;
+        }
+
+        /// <summary>
+        /// Fügt den referencedKeys dieses Standardattributes einen neuen Schlüssel hinzu.
+        /// </summary>
+        /// <param name="key">Dieser Schlüssel wird als referenzierter Schlüssel hinzugefügt.</param>
+        public void AddReferencedKey(string key)
+        {
+            LinkedKeys.Add(key);
+        }
+
+        /// <summary>
+        /// Liest einen Schlüssel aus der Liste der referenzierten Schlüssel und erhöht den Index um eins.
+        /// </summary>
+        /// <returns>
+        /// Gibt ein Tupel zurück:
+        /// 1) Wurde das Ende der Liste erreicht
+        /// 2) Gelesener Schlüssel
+        /// </returns>
+        public Tuple<bool, string> ReadReferencedKey()
+        {
+            Tuple<bool, string> Return;
+
+            if (currentLookIndex < linkedKeys.Count)
+            {
+                Return = new Tuple<bool, string>(true, linkedKeys[currentLookIndex]);
+                currentLookIndex++;
+            }
+            else
+            {
+                Return = new Tuple<bool, string>(false, null);
+                currentLookIndex = 0;
+            }
+
+            return Return;
+        }
+
+        /// <summary>
+        /// Setzt den aktuellen LookIndex auf 0.
+        /// </summary>
+        public void ResetLookIndex() => currentLookIndex = 0;
+
+
+        /// <summary>
+        /// Sichert dieses Standardattribut in die Standardattributtabellen der MainDB.
+        /// </summary>
+        /// <param name="id">Wenn dieser Parameter auf -1 gesetzt wird, dann wird der Wert neu in die Tabelle eingeführt.</param>
+        /// <returns>Die ID unter der das Attribut aktuell in der Tabelle zu finden ist.</returns>
+        public int SaveStandardAttribute(int id)
+        {
+            if(id == -1)
+            {
+
+            }
+            else
+            {
+
+            }
+
+            return id;
+        }
+    }
+}
