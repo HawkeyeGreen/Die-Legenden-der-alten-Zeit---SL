53ada8b03110dfdcbe3501646afb22d3cd5d002a
HawkeyeGreen <thymiana@web.de>
2018-12-27 11:49:49 +0100

    NuGet-Package für Tuple Arbeit am Source-System

---
+++ Die Legenden der Alten Zeit_Lib/World/Ressources/Source.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Die_Legenden_der_Alten_Zeit_Lib.World.Ressources
+{
+    /// <summary>
+    /// Eine Quelle vereint eine Ressource mit ihrer Herkunft.
+    /// Die Quelle berechnet jede Runde die Regeneration ihrer Ressourcen sowie die Auswirkung etwaiger Schäden.
+    /// Dazu können Arbeiter auf dem Tile GatherRessources aufrufen, wobei das Tile wiederum dann einfach GatherRessources auf der Source aufruft.
+    /// </summary>
+    class Source
+    {
+        private int _ID;
+        private int RessourceID;
+        private (string type, int ID) Bound;
+    }
+}
