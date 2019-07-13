c15fbebb71f1d9cc83a6d617088634d72f1140aa
HawkeyeGreen <thymiana@web.de>
2019-07-13 19:32:58 +0200

    Artifacts

---
+++ Die Legenden der Alten Zeit_Lib/Artifacts/Artifact.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Die_Legenden_der_Alten_Zeit_Lib.Artifacts
+{
+    [Serializable]
+    public class Artifact
+    {
+        public string Template { get; set; }
+        public bool Identified { get; set; }
+        public int CurrentStage { get; protected set; }
+        public string Name => ArtifactTemplate.GetArtifactTemplate(Template).Name;
+        public List<string> Tags => ArtifactTemplate.GetArtifactTemplate(Template).Tags;
+        public string RTF => ArtifactTemplate.GetArtifactTemplate(Template).RTF[CurrentStage];
+        public int NextStage => ArtifactTemplate.GetArtifactTemplate(Template).WorkNeededForNextStage[CurrentStage];
+        public List<int> Buffs => ArtifactTemplate.GetArtifactTemplate(Template).Tech_Buffs[CurrentStage];
+        public List<int> Techs => ArtifactTemplate.GetArtifactTemplate(Template).Technologies[CurrentStage];
+        public List<string> Items => ArtifactTemplate.GetArtifactTemplate(Template).Items[CurrentStage];
+        private List<int> visitedStages { get; set; }
+
+
+        public bool HasNextStage()
+        {
+            switch (ArtifactTemplate.GetArtifactTemplate(Template).StageSelectionMode)
+            {
+                case "Random":
+                    return visitedStages.Count < ArtifactTemplate.GetArtifactTemplate(Template).Stages;
+                case "InOrder":
+                default:
+                    return CurrentStage < ArtifactTemplate.GetArtifactTemplate(Template).Stages;
+            }
+        }
+
+        public int GetNextStage()
+        {
+            switch (ArtifactTemplate.GetArtifactTemplate(Template).StageSelectionMode)
+            {
+                case "Random":
+                    List<int> possibleStages = new List<int>();
+                    for(int s = 0; s < ArtifactTemplate.GetArtifactTemplate(Template).Stages; s++)
+                    {
+                        if(!visitedStages.Contains(s))
+                        {
+                            possibleStages.Add(s);
+                        }
+                    }
+                    return possibleStages[RandomHelper.GetHelper().GetGenerator().Next(0, possibleStages.Count)];
+                case "InOrder":
+                default:
+                    return CurrentStage++;
+            }
+        }
+
+        public void SetToStage(int stage)
+        {
+
+        }
+
+
+        /// <summary>
+        /// Loads an Artifact from the given Path.
+        /// Path needs to point to the Directory the Artifact lays in.
+        /// </summary>
+        /// <param name="path">The BaseDirectory of the Artifacts.</param>
+        /// <param name="name">The Name of the Artifact to be loaded.</param>
+        /// <returns></returns>
+        public static Artifact LoadArtifact(string path, string name)
+        {
+            Artifact ReturnMe = new Artifact();
+            XmlSerializer serializer = new XmlSerializer(typeof(Artifact));
+
+            using (Stream file = File.OpenRead(path + "\\" + name + ".xml"))
+            {
+                ReturnMe = (Artifact)serializer.Deserialize(file);
+            }
+            return ReturnMe;
+        }
+
+        public static void SaveArtifact(string path, Artifact artifact)
+        {
+            if (!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+            }
+
+            XmlSerializer serializer = new XmlSerializer(typeof(Artifact));
+
+            using (Stream file = File.OpenWrite(path + "//" + artifact.Name + ".xml"))
+            {
+                serializer.Serialize(file, artifact);
+            }
+        }
+
+        public static List<string> GetArtifacts(string path)
+        {
+            if (!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+            }
+
+            List<string> Return = new List<string>();
+            IEnumerator<string> enumerator = Directory.EnumerateFiles(path).GetEnumerator();
+            while (enumerator.MoveNext())
+            {
+                Return.Add(Path.GetFileNameWithoutExtension(enumerator.Current));
+            }
+            return Return;
+        }
+    }
+}
---
+++ Die Legenden der Alten Zeit_Lib/Artifacts/ArtifactTemplate.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Die_Legenden_der_Alten_Zeit_Lib.Artifacts
+{
+    [Serializable]
+    public class ArtifactTemplate
+    {
+        [NonSerialized]
+        private static Dictionary<string, ArtifactTemplate> Instances = new Dictionary<string, ArtifactTemplate>();
+        [NonSerialized]
+        public static string PATH = AppDomain.CurrentDomain.BaseDirectory + "//ArtifactTemplates";
+        [NonSerialized]
+        public static readonly string SELECTIONMODE_INORDER = "InOrder";
+        [NonSerialized]
+        public static readonly string SELECTIONMODE_RANDOM = "Random";
+
+        public int Stages { get; set; }
+        public string StageSelectionMode { get; set; }
+        public string Name { get; set; }
+        public List<string> RTF { get; set; }
+        public List<string> Tags { get; set; }
+        public List<int> WorkNeededForNextStage { get; set; }
+        public List<List<int>> Technologies { get; set; }
+        public List<List<int>> Tech_Buffs { get; set; }
+        public List<List<string>> Items { get; set; }
+
+        public ArtifactTemplate()
+        {
+            Name = "Undefined";
+            Stages = 1;
+            StageSelectionMode = SELECTIONMODE_INORDER;
+            RTF = new List<string>();
+            WorkNeededForNextStage = new List<int>
+            {
+                1
+            };
+            Technologies = new List<List<int>>
+            {
+                new List<int>()
+            };
+            Tech_Buffs = new List<List<int>>()
+            {
+                new List<int>()
+            };
+            Items = new List<List<string>>()
+            {
+                new List<string>()
+            };
+            Tags = new List<string>();
+        }
+
+        public static ArtifactTemplate GetArtifactTemplate(string name)
+        {
+            if (!Instances.ContainsKey(name))
+            {
+                Instances[name] = LoadArtifactTemplate(name);
+            }
+
+            return Instances[name];
+        }
+
+        /// <summary>
+        /// Loads an Artifact from the given Path.
+        /// Path needs to point to the Directory the Artifact lays in.
+        /// </summary>
+        /// <param name="path">The BaseDirectory of the Artifacts.</param>
+        /// <param name="name">The Name of the Artifact to be loaded.</param>
+        /// <returns></returns>
+        public static ArtifactTemplate LoadArtifactTemplate(string name)
+        {
+            ArtifactTemplate ReturnMe = new ArtifactTemplate();
+            XmlSerializer serializer = new XmlSerializer(typeof(ArtifactTemplate));
+
+            using (Stream file = File.OpenRead(PATH + "\\" + name + ".xml"))
+            {
+                ReturnMe = (ArtifactTemplate)serializer.Deserialize(file);
+            }
+            return ReturnMe;
+        }
+
+        public static void SaveArtifactTemplate(ArtifactTemplate artifact)
+        {
+            if (!Directory.Exists(PATH))
+            {
+                Directory.CreateDirectory(PATH);
+            }
+
+            XmlSerializer serializer = new XmlSerializer(typeof(ArtifactTemplate));
+
+            using (Stream file = File.OpenWrite(PATH + "//" + artifact.Name + ".xml"))
+            {
+                serializer.Serialize(file, artifact);
+            }
+        }
+
+        public static List<string> GetArtifactTemplates()
+        {
+            if (!Directory.Exists(PATH))
+            {
+                Directory.CreateDirectory(PATH);
+            }
+
+            List<string> Return = new List<string>();
+            IEnumerator<string> enumerator = Directory.EnumerateFiles(PATH).GetEnumerator();
+            while (enumerator.MoveNext())
+            {
+                Return.Add(Path.GetFileNameWithoutExtension(enumerator.Current));
+            }
+            return Return;
+        }
+    }
+}
