2a359b204588776f849135e7ada491057fc30ac3
HawkeyeGreen <thymiana@web.de>
2019-07-27 12:37:25 +0200

    Sources und Delivieries

---
+++ Die Legenden der Alten Zeit_Lib/Helper/BoundTo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Die_Legenden_der_Alten_Zeit_Lib.Helper
+{
+    /// <summary>
+    /// Stellt dar, zu was eine Sache (wie eine Lieferung) gebunden ist.
+    /// </summary>
+    public enum BoundTo
+    {
+        // Gebunden an SL-Macht
+        SL,
+        // Gebunden an eine Nation
+        Nation,
+        // Gebunden an eine Fraktion
+        Faction,
+        // Gebunden an eine Seite (Dunkelheit, Licht, Gleichgewicht)
+        Side,
+        // Gaia, Tiere etc
+        Nature,
+        // An Nichts als sich selbst
+        Free
+    }
+}
---
+++ Die Legenden der Alten Zeit_Lib/Ressources/Source.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Die_Legenden_der_Alten_Zeit_Lib.Ressources
+{
+    [Serializable]
+    public class Source
+    {
+        public int TemplateID { get; protected set; }
+        public SourceTemplate Template { get; protected set; }
+        public int Amount { get; set; }
+
+
+        public Dictionary<string, int> DoWorkOnSource(int work) => GetRessources(GetUnits(work));
+
+        private int GetUnits(int work) => Convert.ToInt32(Math.Round(Convert.ToDouble(work / Template.WorkPerUnit), MidpointRounding.AwayFromZero));
+
+        private Dictionary<string, int> GetRessources(int units)
+        {
+            Dictionary<string, int> Ressources = new Dictionary<string, int>();
+            foreach (string res in Template.Ressources)
+            {
+                Ressources[res] = units * Template.RessourcesPerUnit[res];
+            }
+            return Ressources;
+        }
+    }
+}
